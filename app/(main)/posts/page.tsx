985d3648ba32cedcebb93c793d11e679f0cbce8f
HassanThePassionate <maqsoodia777@gmail.com>
2024-06-07 10:02:33 +0500

    Update

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -3,11 +3,13 @@ import PostPagnation from "@/components/posts/PostPagination";
 import Poststable from "@/components/posts/Poststable";
 
 const Postpage = () => {
-    return ( <>
-    <BackButton text="Go Back" link="/"/>
-        <Poststable/>    
-    <PostPagnation/>
-    </> );
-}
- 
+  return (
+    <>
+      <BackButton text='Go Back' link='/' />
+      <Poststable title='Posts' limit={5} />
+      <PostPagnation />
+    </>
+  );
+};
+
 export default Postpage;
